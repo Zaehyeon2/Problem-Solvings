7f3f1ba769049044f108dd0e56eb7eaef87e9a19
Kim JaeHyeon <insam2802@gmail.com>
2022-08-29 18:39:34 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+  int N;
+  char A = '*';
+  scanf("%d", &N);
+  for(int jool = 0; jool < N; jool++){
+    for(int roop = 0; roop < jool; roop++){
+      printf(" ");
+    }
+    for(int roop = jool; roop < N; roop++){
+      printf("*");
+    }
+    printf("\n");
+  }
+
+}
