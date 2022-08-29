a7fc7101ad1da144a56166d815e6d8a5c9e62ffc
Kim JaeHyeon <insam2802@gmail.com>
2022-08-29 20:05:34 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 64 ms, Memory: 29160 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,12 @@
+def fibseq(n):
+    fibs = [0, 1]
+    for k in range(2, n + 1):
+        fibs.append(fibs[k - 1] + fibs[k - 2])
+    return fibs
+
+def fib(n):
+    if n == 0:
+        return 0
+    return fibseq(n)[-1]
+
+print(fib(int(input())))
