327d30ba19d0d02b1015cbefed7188462e88d0d9
Kim JaeHyeon <insam2802@gmail.com>
2022-08-29 18:30:20 +0900

    [Silver V] Title: 소수 찾기, Time: 108 ms, Memory: 29056 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,18 @@
+N = int(input())
+A = input().split(" ")
+sum = 0
+real_sosu = []
+Sosu = [1]* 1000
+Sosu[0], Sosu[1] = 0,0
+for i in range(2,1000):
+    for j in range(i+1,1000):
+        if Sosu[i] == 1 and j%i == 0:
+            Sosu[j] = 0
+for i in range(1000):
+    if Sosu[i] == 1:
+        real_sosu += [i]
+for x in A:
+    if int(x) in real_sosu:
+        sum += 1
+
+print(sum)
